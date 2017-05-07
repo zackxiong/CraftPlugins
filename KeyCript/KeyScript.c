1c9d4a37136b0d30b3c387aa2d3193fe3550e243
d0048 <1198995013@qq.com>
2017-05-07 21:39:56 +0800

    Start KeyCript
    Canceled

---
+++ KeyCript/KeyScript.c
@@ -0,0 +1,9 @@
+#include"KeyScript.h"
+#include<linux/input.h>
+#include<stdio.h>
+#include<iostream>
+
+int main(){
+    printf("1");
+    return 0;
+}
---
+++ KeyCript/debug.sh
@@ -0,0 +1,4 @@
+ls|grep -E '*\.c|*\.h'|grep -v gch|xargs g++
+
+./a.out
+
